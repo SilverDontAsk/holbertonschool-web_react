9fcd6a23446b7bb4d9d45a96adf88b8b3f0c18e2
Silver <silverdontask@SilverDontAsk>
2024-09-23 13:41:48 -0400

    >

--- TypeScript/task_0/webpack.config.js
+++ TypeScript/task_0/webpack.config.js
@@ -4,7 +4,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-    mode: "development",
   entry: "./js/main.ts",
   devtool: "inline-source-map",
   module: {
@@ -33,5 +32,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  mode: 'development',
 };
